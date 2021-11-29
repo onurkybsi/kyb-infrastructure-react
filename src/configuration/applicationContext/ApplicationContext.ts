c0032f16c8aeda9ab4c3400eeed0ca9a67edaed5
onurkybsi <onurbpm@outlook.com>
2021-11-29 23:51:30 +0100

    ApplicationContext was completed

--- src/configuration/applicationContext/ApplicationContext.ts
+++ src/configuration/applicationContext/ApplicationContext.ts
@@ -5,30 +5,58 @@
 
 import ApplicationContextCreationParameters from "./models/ApplicationContextCreationParameters";
 import { readFileSync } from "fs"
+import { HttpClient, StringUtilities, InvalidParameterError } from "../../../../kyb-infrastructure/src/index";
 
 /**
  * Creates a context for the React application with some functionalities for usage of all application scope
  */
 export default class ApplicationContext {
-    private ApplicationParameters: any;
-    private HttpClient: any = {} /** TO-DO: Will be obtain from kyb-infrastructure package */
+    private static instance: ApplicationContext = null;
 
-    constructor(parameters: ApplicationContextCreationParameters) {
+    private ApplicationParameters: any;
+    private HttpClient: HttpClient;
+    private ApplicationContextSetter: (context: ApplicationContext) => void;
+
+    private constructor(parameters: ApplicationContextCreationParameters) {
         this.validateApplicationContextCreationParameters(parameters);
         this.ApplicationParameters = JSON.parse(
             readFileSync(parameters.applicationParametersFilePath).toString()
         );
+        this.HttpClient = new HttpClient(parameters.backendBaseUrl);
+        this.ApplicationContextSetter = parameters.applicationContextSetter;
     }
 
-    private validateApplicationContextCreationParameters = (parameters: any): void => {
-        if (!parameters?.applicationParametersPath)
-            throw Error("Invalid parameter: parameters"); /** TO-DO: Change with InvalidParameterException from kyb-infrastructure */
-        if (
-            parameters.applicationParametersPath.trim().length <= 0 ||
-            parameters.applicationParametersPath.endsWith(".json")
-        )
-            throw Error("Invalid parameter: parameters.applicationParametersPath"); /** TO-DO: Change with InvalidParameterException from kyb-infrastructure */
+    /**
+     * Receives instance of currentapplication context;
+     * @param parameters Parameters for creation of the context
+     * @returns Application context
+     */
+    public static getApplicationContext(parameters: ApplicationContextCreationParameters) {
+        if (this.instance === null) {
+            this.instance = new ApplicationContext(parameters);
+        }
+        return this.instance;
     }
 
+    private validateApplicationContextCreationParameters = (parameters: ApplicationContextCreationParameters): void => {
+        if (!parameters)
+            throw new InvalidParameterError("ApplicationContextCreationParameters", parameters);
+        if (StringUtilities.isValid(parameters.applicationParametersFilePath))
+            throw new InvalidParameterError("params.applicationParametersFilePath", parameters.applicationParametersFilePath);
+        if (!parameters.applicationParametersFilePath.endsWith(".json"))
+            throw new InvalidParameterError("params.applicationParametersFilePath file extension (should be .json)", parameters.applicationParametersFilePath);
+        if (!parameters.applicationContextSetter)
+            throw new InvalidParameterError("params.applicationContextSetter", parameters.applicationContextSetter);
+    }
 
+    /**
+     * Sets the application context, can be used by all application scope
+     * @param applicationContextSetter Function which has operations to update the context 
+     */
+    public setApplicationContext = (applicationContextSetter: (applicationContext: ApplicationContext) => void): void => {
+        if (!applicationContextSetter)
+            throw new InvalidParameterError("applicationContextSetter", applicationContextSetter);
+        applicationContextSetter(this);
+        this.ApplicationContextSetter(this);
+    }
 }
--- src/configuration/applicationContext/models/ApplicationContextCreationParameters.ts
+++ src/configuration/applicationContext/models/ApplicationContextCreationParameters.ts
@@ -1,3 +1,10 @@
+import ApplicationContext from "../ApplicationContext";
+
+/**
+ * Represents parameters objects for creation of ApplicationContext
+ */
 export default interface ApplicationContextCreationParameters {
     applicationParametersFilePath: string;
+    backendBaseUrl: string;
+    applicationContextSetter: (context: ApplicationContext) => void;
 }
